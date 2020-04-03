c50319da76a45805d4d1927f8f2bc5693d17d9ca
wangdingfeng <wangdingfeng@live.com>
2020-04-03 15:40:17 +0800

    工作流

---
+++ spark-flowable/src/main/java/com/spark/platfrom/flowable/SparkFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.spark.platfrom.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkFlowableApplication.class, args);
+    }
+
+}
---
+++ spark-flowable/src/test/java/com/spark/platfrom/flowable/SparkFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.platfrom.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
