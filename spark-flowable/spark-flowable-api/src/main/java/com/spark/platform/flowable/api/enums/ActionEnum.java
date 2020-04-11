69d8838e59deff35b85976ed41950200efff119e
wangdingfeng <wangdingfeng@live.com>
2020-04-11 18:25:15 +0800

    fix

--- spark-flowable/spark-flowable-api/src/main/java/com/spark/platform/flowable/api/enums/ActionEnum.java
+++ spark-flowable/spark-flowable-api/src/main/java/com/spark/platform/flowable/api/enums/ActionEnum.java
@@ -14,9 +14,9 @@ public enum ActionEnum {
 
     COMPLETE("complete","完成任务成功"),
     CLAIM("claim","任务签收成功"),
-    UNCLAIM("claim","任务反签收成功"),
-    DELEGATE("claim","任务委派成功"),
-    RESOLVE("claim"," 任务签收完成,返回任务人完成"),
+    UNCLAIM("unclaim","任务反签收成功"),
+    DELEGATE("delegate","任务委派成功"),
+    RESOLVE("resolve"," 任务签收完成,返回任务人完成"),
     ASSIGNEE("assignee","任务转办成功"),
     SUSPEND("suspend","挂起流程成功"),
     ACTIVATE("activate","激活流程成功");
