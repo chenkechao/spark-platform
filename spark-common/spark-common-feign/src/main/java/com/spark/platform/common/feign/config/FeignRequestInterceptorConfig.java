9a9bdf42cc57991abdb1c34b0e605bf83187fe7b
wangdingfeng <wangdingfeng@live.com>
2020-04-18 17:57:46 +0800

    清洗feign请求头信息 🚒🚒

--- spark-common/spark-common-feign/src/main/java/com/spark/platform/common/feign/config/FeignRequestInterceptorConfig.java
+++ spark-common/spark-common-feign/src/main/java/com/spark/platform/common/feign/config/FeignRequestInterceptorConfig.java
@@ -2,56 +2,38 @@ package com.spark.platform.common.feign.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
 
 /**
  * @author: wangdingfeng
  * @ProjectName: spark-platform
  * @Package: com.spark.platform.common.feign.config
  * @ClassName: FeignRequestInterceptorConfig
- * @Description:
+ * @Description: feign 拦截器 清洗请求头信息
  * @Version: 1.0
  */
 @Component
-public class FeignRequestInterceptorConfig  implements RequestInterceptor {
-
-    private  static  final Logger log = LoggerFactory.getLogger(FeignRequestInterceptorConfig.class);
+@Slf4j
+public class FeignRequestInterceptorConfig implements RequestInterceptor {
 
     private final String AUTHORIZATION_HEADER = "Authorization";
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
-
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-
+        request.getRequestURI();
+        //清洗日志请求头信息
         if (request != null) {
-            log.error("调用feign传递header携带token");
-
-//        只携带token
-           String authorization = request.getHeader(AUTHORIZATION_HEADER);
-//            requestTemplate.header("Authorization", authorization);
-            System.err.println("Authorization :\t\t"+ authorization);
-
-//        携带全部
-            Enumeration<String> headerNames = request.getHeaderNames();
-            if (headerNames != null) {
-                while (headerNames.hasMoreElements()) {
-                    String name = headerNames.nextElement();
-                    String values = request.getHeader(name);
-                    requestTemplate.header(name, values);
-                    log.debug("name ：\t\t" + name);
-                    log.debug("values ： \t\t" + values);
-
-                }
-            }
+            // 只携带token
+            String authorization = request.getHeader(AUTHORIZATION_HEADER);
+            log.info("Authorization :\t\t" + authorization);
+            requestTemplate.header(AUTHORIZATION_HEADER, authorization);
 
         }
 
